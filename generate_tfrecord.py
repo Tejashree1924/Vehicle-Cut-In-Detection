6b54fbc799c1ce5a9f7ae3dfe2a12da707423c16
Tejashree1924 <160414969+Tejashree1924@users.noreply.github.com>
2024-07-06 23:37:44 +0530

    generate_tfrecord.py

--- generate_tfrecord.py
+++ generate_tfrecord.py
@@ -1,41 +1,42 @@
-"""
-Usage:
-  # From tensorflow/models/research/
-  # Create train data:
-  python generate_tfrecord.py --csv_input=images/train_labels.csv --output_path=train.record
-
-  # Create test data:
-  python generate_tfrecord.py --csv_input=images/test_labels.csv --output_path=test.record
-"""
-from __future__ import division
-from __future__ import print_function
-from __future__ import absolute_import
-
 import os
 import io
 import pandas as pd
 import tensorflow as tf
-
 from PIL import Image
 from object_detection.utils import dataset_util
 from collections import namedtuple, OrderedDict
+import xml.etree.ElementTree as ET
 
-flags = tf.app.flags
-flags.DEFINE_string('csv_input', '', 'Path to the CSV input')
-flags.DEFINE_string('output_path', '', 'Path to output TFRecord')
-flags.DEFINE_string('image_dir', '', 'Path to images')
-FLAGS = flags.FLAGS
+# Paths
+IMAGE_DIR = 'images/'  # Directory where extracted frames are saved
+ANNOTATIONS_DIR = 'annotations/'  # Directory where XML annotations are saved
+TFRECORD_FILENAME = 'train.record'
 
-# TO-DO replace this with label map
+# Define label map
+LABEL_MAP = {'car': 1, 'bus': 2, 'truck': 3}  # Add all classes here
+
+def xml_to_csv(path):
+    xml_list = []
+    for xml_file in glob.glob(path + '/*.xml'):
+        tree = ET.parse(xml_file)
+        root = tree.getroot()
+        for member in root.findall('object'):
+            value = (root.find('filename').text,
+                     int(root.find('size')[0].text),
+                     int(root.find('size')[1].text),
+                     member[0].text,
+                     int(member[4][0].text),
+                     int(member[4][1].text),
+                     int(member[4][2].text),
+                     int(member[4][3].text)
+                     )
+            xml_list.append(value)
+    column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
+    xml_df = pd.DataFrame(xml_list, columns=column_name)
+    return xml_df
+
 def class_text_to_int(row_label):
-    if row_label == 'car':
-        return 1
-    elif row_label == 'bus':
-        return 2
-    elif row_label == 'truck':
-        return 3
-    else:
-        None
+    return LABEL_MAP[row_label]
 
 def split(df, group):
     data = namedtuple('data', ['filename', 'object'])
@@ -43,14 +44,14 @@ def split(df, group):
     return [data(filename, gb.get_group(x)) for filename, x in zip(gb.groups.keys(), gb.groups)]
 
 def create_tf_example(group, path):
-    with tf.gfile.GFile(os.path.join(path, '{}'.format(group.filename)), 'rb') as fid:
+    with tf.io.gfile.GFile(os.path.join(path, '{}'.format(group.filename)), 'rb') as fid:
         encoded_jpg = fid.read()
     encoded_jpg_io = io.BytesIO(encoded_jpg)
     image = Image.open(encoded_jpg_io)
     width, height = image.size
 
     filename = group.filename.encode('utf8')
-    image_format = b'jpeg'
+    image_format = b'jpg'
     xmins = []
     xmaxs = []
     ymins = []
@@ -83,17 +84,16 @@ def create_tf_example(group, path):
     return tf_example
 
 def main(_):
-    writer = tf.python_io.TFRecordWriter(FLAGS.output_path)
-    path = os.path.join(FLAGS.image_dir)
-    examples = pd.read_csv(FLAGS.csv_input)
+    writer = tf.io.TFRecordWriter(TFRECORD_FILENAME)
+    path = os.path.join(IMAGE_DIR)
+    examples = xml_to_csv(ANNOTATIONS_DIR)
     grouped = split(examples, 'filename')
     for group in grouped:
         tf_example = create_tf_example(group, path)
         writer.write(tf_example.SerializeToString())
 
     writer.close()
-    output_path = os.path.join(os.getcwd(), FLAGS.output_path)
-    print('Successfully created the TFRecord file: {}'.format(output_path))
+    print('Successfully created the TFRecord file: {}'.format(TFRECORD_FILENAME))
 
 if __name__ == '__main__':
-    tf.app.run()
+    tf.compat.v1.app.run()
